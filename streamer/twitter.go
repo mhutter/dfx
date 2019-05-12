d6577b4da5da9a025d66184e119c661720ef211d
Manuel Hutter <manuel@hutter.io>
2019-05-12 16:42:02 +0200

    Ensure Deployable already contains required HTML

--- streamer/twitter.go
+++ streamer/twitter.go
@@ -1,6 +1,9 @@
 package streamer
 
 import (
+	"fmt"
+	"log"
+
 	"github.com/dghubble/go-twitter/twitter"
 	"github.com/dghubble/oauth1"
 	"github.com/mhutter/dfx"
@@ -43,7 +46,12 @@ func (t *Twitter) Start() (<-chan *dfx.Deployable, error) {
 
 	demux := twitter.NewSwitchDemux()
 	demux.Tweet = func(tweet *twitter.Tweet) {
-		d := dfx.NewFromTweet(tweet)
+		d, err := t.DeployableFromTweet(tweet)
+		if err != nil {
+			log.Printf("ERROR: %s", err)
+			return
+		}
+
 		ch <- d
 	}
 
@@ -62,4 +70,28 @@ func (t *Twitter) Start() (<-chan *dfx.Deployable, error) {
 // Stop stops the streaming client
 func (t *Twitter) Stop() {
 	t.stream.Stop()
+}
+
+// DeployableFromTweet converts a tweet to a Deployable
+func (t *Twitter) DeployableFromTweet(tweet *twitter.Tweet) (*dfx.Deployable, error) {
+	surl := fmt.Sprintf(
+		"https://twitter.com/%s/status/%s",
+		tweet.User.ScreenName,
+		tweet.IDStr,
+	)
+
+	e, _, err := t.client.Statuses.OEmbed(&twitter.StatusOEmbedParams{
+		URL: surl,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return &dfx.Deployable{
+		Title:     e.AuthorName,
+		Content:   e.HTML,
+		From:      tweet.User.ScreenName,
+		SourceUrl: surl,
+		Source:    "twitter",
+	}, nil
 }
